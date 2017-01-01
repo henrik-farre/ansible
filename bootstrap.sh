4d0f78a06c36f92fb9432514c086946136a33fea
Henrik Farre <hfa@ordbogen.com>
2017-01-01 15:00:52 +0100

    use workstation instead of localhost

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook -i hosts.ini bootstrap.yml --limit localhost
+ansible-playbook -i hosts.ini bootstrap.yml --limit workstation
--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook -i hosts.ini setup.yml --limit localhost
+ansible-playbook -i hosts.ini setup.yml --limit workstation
